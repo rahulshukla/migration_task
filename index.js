4694e08525d68bad22feffb9d225a8c3211a8806
Rahul Shukla <rahul.shukla13@gmail.com>
2020-04-07 15:59:03 +0530

    getting API endpoint from environment

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const path = require('path');
 const generateList  = require(path.join(__dirname, 'bin', 'getOldContent'))
 const upgradeQumlQuestion  = require(path.join(__dirname, 'bin', 'updateQUMLdata'))
 const publishContent  = require(path.join(__dirname, 'bin', 'createItemset'))
+const constant  = require(path.join(__dirname,  './constants'))
 
 //  to generate the list of content which doesn't have itemset attached
 // generateList.generateContentList();
 
 // fetch QUML version 0.5 version question attached to content and upgrade to QUML 1.0
-upgradeQumlQuestion.updateQumlQuestion();
+// upgradeQumlQuestion.updateQumlQuestion();
 
 // create Itemset from items/question 
 // update content with itemset
 // publish content if status is live
-publishContent.publishContent();
+// publishContent.publishContent();
+
+console.log(constant.apiEndpointUrl);
 
