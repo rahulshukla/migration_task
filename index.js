815341d5baaf310c468909868f475eac0e4a6370
Rahul Shukla <rahul.shukla13@gmail.com>
2020-05-19 16:53:29 +0530

    Preprod deployment

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ switch(args.x) {
     case 'get': // Get content details
         // console.log("\u001B[1m" + "\u001B[32m" +"Generating CSV for old QUML content" + "\u001B[0m")
         // to generate the list of content which doesn't have itemset attached
-        // generateList.generateContentList();
+        generateList.generateContentList();
       break;
     case 'update': // update content details
         console.log("\u001B[1m" + "\u001B[32m" +"Updating old QUML content to New QUML content" + "\u001B[0m")
         // fetch QUML version 0.5 version question attached to content and upgrade to QUML 1.0
-        updateQumlQuestion.updateQumlQuestion();
+        // updateQumlQuestion.updateQumlQuestion();
       break;
     case 'publish':  // publish content details
         console.log("\u001B[1m" + "\u001B[32m" +"Publishing updated content" + "\u001B[0m")
         // create Itemset from items/question 
         // update content with itemset
         // publish content if status is live
-        publishContent.publishContent();
+        // publishContent.publishContent();
         break;
     default:
       console.log("\u001B[1m" + "\u001B[32m" +"Nothing to do check the script" + "\u001B[0m")
